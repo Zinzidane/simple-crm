cd62f62e5b366ff531299339d937631267239ace
Zinzidane <ya-gans@mail.ru>
2018-08-27 17:53:10 +0300

    Создает базовую серверную часть

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const authRoutes = require('./routes/auth');
+const analyticsRoutes = require('./routes/analytics');
+const categoryRoutes = require('./routes/category');
+const orderRoutes = require('./routes/order');
+const positionRoutes = require('./routes/position');
+const app = express();
+const keys = require('./config/keys');
+
+const options = {
+  useNewUrlParser: true
+};
+
+mongoose.connect(keys.mongoURI, options)
+  .then(() => console.log('Database connected'))
+  .catch(error => console.log(error));
+
+
+app.use(passport.initialize());
+require('./middleware/passport')(passport);
+
+app.use(require('morgan')('dev'));
+app.use('/uploads', express.static('uploads'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(require('cors')());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/analytics', analyticsRoutes);
+app.use('/api/category', categoryRoutes);
+app.use('/api/order', orderRoutes);
+app.use('/api/position', positionRoutes);
+
+module.exports = app;
