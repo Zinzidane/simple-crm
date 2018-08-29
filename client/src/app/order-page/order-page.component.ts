7a26648b3d00cc024c5e05345820b2d34b48b2b1
Zinzidane <ya-gans@mail.ru>
2018-08-29 20:29:17 +0300

    Реализует отображение заказов

--- client/src/app/order-page/order-page.component.ts
+++ client/src/app/order-page/order-page.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy, AfterViewInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { MaterialService, MaterialInstance } from '../shared/classes/material.service';
 
 @Component({
   selector: 'app-order-page',
   templateUrl: './order-page.component.html',
   styleUrls: ['./order-page.component.css']
 })
-export class OrderPageComponent implements OnInit {
+export class OrderPageComponent implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChild('modal') modalRef: ElementRef
 
-  constructor() { }
+  modal: MaterialInstance
+  isRoot: boolean
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.isRoot = this.router.url === '/order';
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isRoot = this.router.url === '/order';
+      }
+    });
+  }
+
+  ngAfterViewInit() {
+    this.modal = MaterialService.initModal(this.modalRef);
+  }
+
+  ngOnDestroy() {
+    this.modal.destroy();
+  }
+
+  open() {
+    this.modal.open();
+  }
+
+  cancel() {
+    this.modal.close();
+  }
+
+  submit() {
+    this.modal.close();
   }
 
 }
