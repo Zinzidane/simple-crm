aceb7dca6a94f33159dde17edeae7b0a8c651a47
Zinzidane <ya-gans@mail.ru>
2018-08-30 12:26:37 +0300

    Реализует открытие модального окна с данными для заказа

--- client/src/app/history-page/history-list/history-list.component.ts
+++ client/src/app/history-page/history-list/history-list.component.ts
@@ -1,17 +1,41 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, OnDestroy, AfterViewInit } from '@angular/core';
 import { Order } from '../../shared/interfaces';
+import { MaterialInstance, MaterialService } from '../../shared/classes/material.service';
 
 @Component({
   selector: 'app-history-list',
   templateUrl: './history-list.component.html',
   styleUrls: ['./history-list.component.css']
 })
-export class HistoryListComponent implements OnInit {
+export class HistoryListComponent implements OnDestroy, AfterViewInit {
   @Input() orders: Order[];
+  @ViewChild('modal') modalRef: ElementRef;
+
+  modal: MaterialInstance;
+  selectedOrder: Order;
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.modal = MaterialService.initModal(this.modalRef);
   }
 
+  ngOnDestroy() {
+    this.modal.destroy();
+  }
+
+  computePrice(order: Order): number {
+    return order.list.reduce((total, item) => {
+      return total += item.quantity * item.cost;
+    }, 0);
+  }
+
+  selectOrder(order: Order) {
+    this.selectedOrder = order;
+    this.modal.open();
+  }
+
+  closeModal() {
+    this.modal.close();
+  }
 }
